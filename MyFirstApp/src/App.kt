137dd2f96d386313322831a330871eb2a2cc8b8b
tomashevich <evg.tomash@gmail.com>
2024-09-02 17:17:35 +0300

    Finish lesson 1

---
+++ MyFirstApp/src/App.kt
@@ -0,0 +1,34 @@
+fun main (args: Array<String>) {
+
+    //    WHILE
+//    var x = 1
+//    println("Before the loop. x = $x.")
+//    while (x < 4) {
+//        println("In the loop. x = $x.")
+//        x = x + 1
+//    }
+//    println("After the loop. x = $x.")
+
+    //    IF
+//    val x = 3
+//    val y = 1
+//    if (x > y) {
+//        println("x is greater than y")
+//    }
+//    println("This line runs no matter what")
+
+    //    IF + ELSE
+//    val x = 3
+//    val y = 1
+//    if (x > y) {
+//        println("x is greater than y")
+//    } else {
+//        println("x is not greater than y")
+//    }
+//    println("This line runs no matter what")
+
+    val x = 3
+    val y = 1
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")
+}
