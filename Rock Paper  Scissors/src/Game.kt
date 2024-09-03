e26ba9f323c49832abe84dfed70215466671ad90
Evgeniy Tomashevich <evg.tomash@gmail.com>
2024-09-03 15:49:12 +0300

    WIP

--- Rock Paper  Scissors/src/Game.kt
+++ Rock Paper  Scissors/src/Game.kt
@@ -1,3 +1,6 @@
 fun main() {
-
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
 }
+
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[ (Math.random() * optionsParam.size).toInt()]
